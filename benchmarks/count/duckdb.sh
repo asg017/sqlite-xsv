09df49cb5fbf2a0069cc40c0f466f49b804f5aaa
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-01-10 17:06:34 -0800

    duck fix

--- benchmarks/count/duckdb.sh
+++ benchmarks/count/duckdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 duckdb :memory: \
-'select count(*) from "../_data/totals.csv"'
+"select count(*) from read_csv('../_data/totals.csv', header=true, columns={'id': 'text','name': 'text','date': 'date','county': 'text','fips': 'text','confirmed_cases': 'int64','note': 'text','population': 'int'});"
--- benchmarks/count/duckdb_parallel.sh
+++ benchmarks/count/duckdb_parallel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 duckdb :memory: \
   'SET experimental_parallel_csv=true;' \
-  'select count(*) from "../_data/totals.csv"'
+  "select count(*) from read_csv('../_data/totals.csv', header=true, columns={'id': 'text','name': 'text','date': 'date','county': 'text','fips': 'text','confirmed_cases': 'int64','note': 'text','population': 'int'});"
